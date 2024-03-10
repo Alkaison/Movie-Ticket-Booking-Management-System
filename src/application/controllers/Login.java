7dd402e8324f696fb16099eb3a91fbf35ea592ef
Alkaison <505ganeshmourya@gmail.com>
2024-03-10 14:05:43 +0530

    feat: linked FXML resources for login screen

--- src/application/controllers/Login.java
+++ src/application/controllers/Login.java
@@ -1,13 +1,24 @@
 package application.controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Login {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+
 	@FXML
 	private AnchorPane loginFormContainer;
 
@@ -29,11 +40,22 @@ public class Login {
 		System.out.println("Password: " + password);
 	}
 
-	public void signUp(ActionEvent e) {
-		System.out.println("Sign Up Text Clicked");
+	public void signUp(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/application/fxml/SignUp.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
 	}
 
-	public void forgetPassword(ActionEvent e) {
-		System.out.println("Forget Password Text Clicked");
+	public void forgetPassword(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/application/fxml/ForgotPassword.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
 	}
+
 }
