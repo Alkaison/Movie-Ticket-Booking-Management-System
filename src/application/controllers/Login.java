3508ec5cdb2166cdd37d319fb60a45bdd2e896b9
Alkaison <505ganeshmourya@gmail.com>
2024-03-22 22:46:37 +0530

    fix: code formatting and event imports

--- src/application/controllers/Login.java
+++ src/application/controllers/Login.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
--- src/application/controllers/selectPayment.java
+++ src/application/controllers/selectPayment.java
@@ -1,6 +1,5 @@
 package application.controllers;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -10,52 +9,51 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+public class SelectPayment implements Initializable {
 
-public class selectPayment implements Initializable{
+	@FXML
+	private Label MovieTitle;
 
-    @FXML
-    private Label MovieTitle;
-    @FXML
-    private Label Price;
+	@FXML
+	private Label Price;
 
-    @FXML
-    private Button paymentButton1;
-    @FXML
-    private Button paymentButton2;
-    @FXML
-    private Button paymentButton3;
+	@FXML
+	private Button paymentButton1;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        setTitleAndPrice();
-    }
+	@FXML
+	private Button paymentButton2;
 
-    @FXML
-    void hover(MouseEvent event) {
-        paymentButton1.setOnMouseEntered(event1 -> paymentButton1.setStyle("-fx-scale-x: 1.1;"));
-        paymentButton2.setOnMouseEntered(event1 -> paymentButton2.setStyle("-fx-scale-x: 1.1;"));
-        paymentButton3.setOnMouseEntered(event1 -> paymentButton3.setStyle("-fx-scale-x: 1.1;"));
-    }
+	@FXML
+	private Button paymentButton3;
 
-    @FXML
-    void Removehover(MouseEvent event) {
-        paymentButton1.setOnMouseExited(event1 -> paymentButton1.setStyle("-fx-scale-x: 1.0;"));
-        paymentButton2.setOnMouseExited(event1 -> paymentButton2.setStyle("-fx-scale-x: 1.0;"));
-        paymentButton3.setOnMouseExited(event1 -> paymentButton3.setStyle("-fx-scale-x: 1.0;"));
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setTitleAndPrice();
+	}
 
-    @FXML
-    void setTitleAndPrice() {
-        // Code for Setting the  Movie Title From Database
-        MovieTitle.setText("Movie Title");
-        Price.setText("Total Amount: Rs XXX/- ");
-    }
+	@FXML
+	void hover(MouseEvent event) {
+		paymentButton1.setOnMouseEntered(event1 -> paymentButton1.setStyle("-fx-scale-x: 1.1;"));
+		paymentButton2.setOnMouseEntered(event1 -> paymentButton2.setStyle("-fx-scale-x: 1.1;"));
+		paymentButton3.setOnMouseEntered(event1 -> paymentButton3.setStyle("-fx-scale-x: 1.1;"));
+	}
 
-    @FXML
-    void onaction(ActionEvent event) {
-        // Page Changing Logic ----
-    }
+	@FXML
+	void Removehover(MouseEvent event) {
+		paymentButton1.setOnMouseExited(event1 -> paymentButton1.setStyle("-fx-scale-x: 1.0;"));
+		paymentButton2.setOnMouseExited(event1 -> paymentButton2.setStyle("-fx-scale-x: 1.0;"));
+		paymentButton3.setOnMouseExited(event1 -> paymentButton3.setStyle("-fx-scale-x: 1.0;"));
+	}
+
+	@FXML
+	void setTitleAndPrice() {
+		// Code for Setting the Movie Title From Database
+		MovieTitle.setText("Movie Title");
+		Price.setText("Total Amount: Rs XXX/- ");
+	}
+
+	@FXML
+	void onaction(ActionEvent event) {
+		// Page Changing Logic ----
+	}
 }
-
-
-
