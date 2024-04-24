df80193112546c968740ef572f148509dc60dc75
Alkaison <505ganeshmourya@gmail.com>
2024-04-24 09:47:18 +0530

    chore: fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			// path to Login FXML file
-			String fxmlResource = "fxml/CreditCard.fxml";
+			String fxmlResource = "fxml/WelcomeScreeen.fxml";
 
 			// initializing stage and scene
 			Parent root = FXMLLoader.load(getClass().getResource(fxmlResource));
